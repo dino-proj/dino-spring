1057693f29657b6b47cca43e9649ddb0eb27bf71
卢学裕 <luxueyu@botbrain.ai>
2021-10-01 22:42:48 +0800

    增加作者信息

--- dino-spring-core/src/main/java/org/dinospring/core/vo/VoBase.java
+++ dino-spring-core/src/main/java/org/dinospring/core/vo/VoBase.java
@@ -1,11 +1,11 @@
 // Copyright 2021 dinospring.cn
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,17 +19,37 @@ import java.util.Date;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+/**
+ *
+ * @author tuuboo
+ */
 public interface VoBase<K extends Serializable> extends Serializable {
 
+  /**
+   * ID
+   * @return id
+   */
   @Schema(description = "ID")
   K getId();
 
+  /**
+   * 对象创建时间
+   * @return 创建时间
+   */
   @Schema(description = "创建时间")
   Date getCreateAt();
 
+  /**
+   * 对象最后更新时间
+   * @return 更新时间
+   */
   @Schema(description = "最后更新时间")
   Date getUpdateAt();
 
-  @Schema(description = "状态")
+  /**
+   * 对象状态码
+   * @return
+   */
+  @Schema(description = "状态码")
   Integer getStatus();
 }

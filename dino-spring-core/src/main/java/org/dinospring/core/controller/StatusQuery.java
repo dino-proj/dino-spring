236225728834313bab8072322ed3e279f8cbc52b
卢学裕 <luxueyu@botbrain.ai>
2021-10-26 23:39:42 +0800

    增加状态查询类

---
+++ dino-spring-core/src/main/java/org/dinospring/core/controller/StatusQuery.java
@@ -0,0 +1,36 @@
+// Copyright 2021 dinospring.cn
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.dinospring.core.controller;
+
+import com.botbrain.dino.sql.builder.SelectSqlBuilder;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.dinospring.core.service.CustomQuery;
+import org.dinospring.data.domain.EntityBase;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class StatusQuery implements CustomQuery {
+  @Schema(description = "状态，默认查询全部")
+  private Integer[] status;
+
+  @Override
+  public SelectSqlBuilder buildSql(SelectSqlBuilder sql) {
+    return sql.inIf(ArrayUtils.isNotEmpty(status), EntityBase.Fields.status, status);
+  }
+
+}

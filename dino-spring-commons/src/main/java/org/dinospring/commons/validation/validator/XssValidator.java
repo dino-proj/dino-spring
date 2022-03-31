076ac5387d3e601ad762fef929e37a89f289f081
卢学裕 <luxueyu@botbrain.ai>
2022-04-01 00:22:19 +0800

    增加Xss，中国手机号验证

---
+++ dino-spring-commons/src/main/java/org/dinospring/commons/validation/validator/XssValidator.java
@@ -0,0 +1,36 @@
+// Copyright 2022 dinospring.cn
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.dinospring.commons.validation.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.apache.commons.lang3.StringUtils;
+import org.dinospring.commons.validation.constraints.Xss;
+
+public class XssValidator implements ConstraintValidator<Xss, String> {
+
+  @Override
+  public void initialize(Xss constraintAnnotation) {
+  }
+
+  @Override
+  public boolean isValid(String value, ConstraintValidatorContext context) {
+    if (StringUtils.isBlank(value)) {
+      return true;
+    }
+    return !value.contains("<") && !value.contains(">");
+  }
+}

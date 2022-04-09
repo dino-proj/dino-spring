0502e2bee426f1cdad72655982f85caa6691b976
liujie <liujiehaobang@163.com>
2022-04-09 21:56:14 +0800

    格式化

--- dino-spring-core/src/main/java/org/dinospring/core/modules/scope/ScopeRepository.java
+++ dino-spring-core/src/main/java/org/dinospring/core/modules/scope/ScopeRepository.java
@@ -1,11 +1,11 @@
 // Copyright 2022 dinospring.cn
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,6 +29,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ScopeRepository extends CrudRepositoryBase<ScopeEntity, Long> {
 
+  /**
+   * 通过哈希值获取规则
+   * @param hash
+   * @return
+   */
   @Query("FROM #{#entityName} e WHERE e.ruleHash = :hash")
   Optional<Long> getByRuleHash(String hash);
 }

6648102cbbc8627dd2fab97002f7c66b6d369144
liujie <liujiehaobang@163.com>
2022-05-05 16:04:30 +0800

    导入导出

---
+++ dino-spring-core/src/main/java/org/dinospring/core/modules/importandexport/dataimport/ImportController.java
@@ -0,0 +1,74 @@
+// Copyright 2021 dinospring.cn
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.dinospring.core.modules.importandexport.dataimport;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.dinospring.commons.response.Response;
+import org.dinospring.commons.utils.TypeUtils;
+import org.dinospring.core.annotion.param.ParamTenant;
+import org.dinospring.core.modules.importandexport.handler.DataImportHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import io.swagger.v3.oas.annotations.Operation;
+
+/**
+ * @author JL
+ * @param <T>
+ * @Date: 2021/9/30
+ */
+public interface ImportController<S extends DataImportHandler<T>, T> {
+
+  /**
+   * 数据存储接口
+   *
+   * @return
+   */
+  S importHandler();
+
+  /**
+   * 文件导入
+   * @param file 文件流
+   * @return
+   * @throws IOException
+   */
+  @Operation(summary = "导入")
+  @ParamTenant
+  @PostMapping("import")
+  default Response<Boolean> dataImport(@RequestParam("file") MultipartFile file) throws IOException {
+    DataImport.doRead(file.getInputStream(), TypeUtils.getGenericParamClass(this, ImportController.class, 1),
+        e -> {
+          importHandler().importData((List<T>) e);
+        });
+    return Response.success();
+  }
+
+  /**
+   * 文件导入
+   * @param pathName 文件目录
+   * @return
+   * @throws IOException
+   */
+  default Response<Boolean> dataImport(String pathName) throws IOException {
+    DataImport.doRead(pathName, TypeUtils.getGenericParamClass(this, DataImportHandler.class, 1), e -> {
+      importHandler().importData((List<T>) e);
+    });
+    return Response.success();
+  }
+
+}

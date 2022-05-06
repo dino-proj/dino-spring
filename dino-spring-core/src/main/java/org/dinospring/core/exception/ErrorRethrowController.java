f4ab19aa747430ba3bc930b97ef95522f31a2223
卢学裕 <luxueyu@botbrain.ai>
2022-05-06 19:53:34 +0800

    增加Filter中的异常处理

---
+++ dino-spring-core/src/main/java/org/dinospring/core/exception/ErrorRethrowController.java
@@ -0,0 +1,37 @@
+// Copyright 2022 dinospring.cn
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.dinospring.core.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 将filter的异常重新抛出，让其被全局异常处理器捕获
+ * @author tuuboo
+ * @date 2022-05-06 19:46:56
+ */
+@Controller
+public class ErrorRethrowController implements ErrorController {
+
+  @RequestMapping("/error")
+  public void handleError(HttpServletRequest request) throws Throwable {
+    if (request.getAttribute("javax.servlet.error.exception") != null) {
+      throw (Throwable) request.getAttribute("javax.servlet.error.exception");
+    }
+  }
+}

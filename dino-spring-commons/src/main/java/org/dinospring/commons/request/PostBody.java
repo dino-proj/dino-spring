24745a85c4f8e2478e7b9df491e0b4a38d71dac2
卢学裕 <luxueyu@botbrain.ai>
2022-05-30 17:08:36 +0800

    移除PostBody里的用户信息字段

--- dino-spring-commons/src/main/java/org/dinospring/commons/request/PostBody.java
+++ dino-spring-commons/src/main/java/org/dinospring/commons/request/PostBody.java
@@ -26,14 +26,7 @@ import lombok.Data;
 
 @Data
 public class PostBody<T> {
-
-  @Schema(description = "登录用户的ID")
-  private String uid;
-
-  @Schema(description = "登录用户的类型")
-  private String utype;
-
-  @Schema(description = "用户的GUID", nullable = true)
+  @Schema(description = "设备GUID", nullable = true)
   private String guid;
 
   @Schema(description = "客户端的平台，PC、APP、WX、H5等")

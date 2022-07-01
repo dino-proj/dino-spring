3903f22b5548793389b6512bdf20fe17585fd265
卢学裕 <luxueyu@botbrain.ai>
2022-07-01 20:52:00 +0800

    增加author信息

--- dino-spring-commons/src/main/java/org/dinospring/commons/bean/Property.java
+++ dino-spring-commons/src/main/java/org/dinospring/commons/bean/Property.java
@@ -1,3 +1,17 @@
+// Copyright 2022 dinospring.cn
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package org.dinospring.commons.bean;
 
 import java.lang.annotation.Annotation;
@@ -20,6 +34,12 @@ import org.springframework.util.ReflectionUtils;
 import org.springframework.util.StringUtils;
 
 import com.fasterxml.jackson.annotation.JsonView;
+
+/**
+ *
+ * @author tuuboo
+ * @date 2022-07-01 20:51:22
+ */
 
 public final class Property {
 

a66ee7194160eeaba0e70f4805fbcc9e542ab80c
Cody Lu <tuuboo@gmail.com>
2023-09-17 05:38:46 +0800

    升级依赖库版本到最新

--- dino-spring-core/src/main/java/org/dinospring/core/modules/importandexport/dataexport/DataExport.java
+++ dino-spring-core/src/main/java/org/dinospring/core/modules/importandexport/dataexport/DataExport.java
@@ -14,6 +14,9 @@
 
 package org.dinospring.core.modules.importandexport.dataexport;
 
+import static com.alibaba.excel.EasyExcelFactory.write;
+import static com.alibaba.excel.EasyExcelFactory.writerSheet;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -22,12 +25,11 @@ import java.io.OutputStream;
 import java.util.List;
 import java.util.Set;
 
-import com.alibaba.excel.EasyExcel;
-import com.alibaba.excel.ExcelWriter;
-import com.alibaba.excel.write.metadata.WriteSheet;
-
 import org.dinospring.commons.utils.LambdaUtils;
 import org.dinospring.core.modules.importandexport.handler.DataExportHandler;
+
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
 
 /**
  * @author JL
@@ -67,9 +69,9 @@ public class DataExport {
     // 这里 需要指定写用哪个class去写
     ExcelWriter excelWriter = null;
     try {
-      excelWriter = EasyExcel.write(outputStream, cls).build();
-      WriteSheet writeSheet = EasyExcel.writerSheet(0).build();
-      excelWriter.write(() -> cb.importData(), writeSheet);
+      excelWriter = write(outputStream, cls).build();
+      WriteSheet writeSheet = writerSheet(0).build();
+      excelWriter.write(cb::importData, writeSheet);
     } finally {
       // 千万别忘记finish 会帮忙关闭流
       if (excelWriter != null) {
@@ -84,7 +86,7 @@ public class DataExport {
     // 这里需要注意 在使用ExcelProperty注解的使用，如果想不空列则需要加入order字段，而不是index,order会忽略空列，然后继续往后，而index，不会忽略空列，在第几列就是第几列。
 
     // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
-    EasyExcel.write(fileName, cls).excludeColumnFiledNames(excludeColumnFiledNames).sheet(0)
+    write(fileName, cls).excludeColumnFieldNames(excludeColumnFiledNames).sheet(0)
         .doWrite(list);
   }
 
@@ -93,7 +95,7 @@ public class DataExport {
     // 这里需要注意 在使用ExcelProperty注解的使用，如果想不空列则需要加入order字段，而不是index,order会忽略空列，然后继续往后，而index，不会忽略空列，在第几列就是第几列。
 
     // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
-    EasyExcel.write(fileName, cls).includeColumnFiledNames(includeColumnFiledNames).sheet(0)
+    write(fileName, cls).includeColumnFieldNames(includeColumnFiledNames).sheet(0)
         .doWrite(list);
   }
 
@@ -101,12 +103,12 @@ public class DataExport {
     ExcelWriter excelWriter = null;
     try {
       // 这里 指定文件
-      excelWriter = EasyExcel.write(fileName).build();
+      excelWriter = write(fileName).build();
       // 去调用写入,这里我调用了五次，实际使用时根据数据库分页的总的页数来。这里最终会写到5个sheet里面
       ExcelWriter finalExcelWriter = excelWriter;
       LambdaUtils.forEach(clsList, (index, item) -> {
         // 每次都要创建writeSheet 这里注意必须指定sheetNo 而且sheetName必须不一样。这里注意DemoData.class 可以每次都变，我这里为了方便 所以用的同一个class 实际上可以一直变
-        WriteSheet writeSheet = EasyExcel.writerSheet(index, "sheet" + index).head(item).build();
+        WriteSheet writeSheet = writerSheet(index, "sheet" + index).head(item).build();
         // 分页去数据库查询数据 这里可以去数据库查询每一页的数据
         finalExcelWriter.write(list, writeSheet);
       });

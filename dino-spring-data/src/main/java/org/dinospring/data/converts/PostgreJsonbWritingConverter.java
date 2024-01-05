1fab1326e14487aba4f6d83af311c41646fd8a30
Cody Lu <tuuboo@gmail.com>
2024-01-06 07:51:48 +0800

    Add Object to PGobject Convert

--- dino-spring-data/src/main/java/org/dinospring/data/converts/PostgreJsonbWritingConverter.java
+++ dino-spring-data/src/main/java/org/dinospring/data/converts/PostgreJsonbWritingConverter.java
@@ -1,16 +1,5 @@
-// Copyright 2021 dinodev.cn
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2024 dinosdev.cn.
+// SPDX-License-Identifier: Apache-2.0
 
 package org.dinospring.data.converts;
 
@@ -20,9 +9,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.postgresql.util.PGobject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -31,6 +17,9 @@ import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.core.convert.converter.GenericConverter;
 import org.springframework.data.convert.WritingConverter;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -52,6 +41,7 @@ public class PostgreJsonbWritingConverter implements GenericConverter {
   public Set<ConvertiblePair> getConvertibleTypes() {
     return Set.of(new ConvertiblePair(Collection.class, PGobject.class), //
         new ConvertiblePair(Map.class, PGobject.class), //
+        new ConvertiblePair(Object.class, PGobject.class), //
         new ConvertiblePair(Object[].class, PGobject.class), //
         new ConvertiblePair(String[].class, PGobject.class), //
         new ConvertiblePair(Boolean[].class, PGobject.class), //
@@ -67,7 +57,7 @@ public class PostgreJsonbWritingConverter implements GenericConverter {
       if (Objects.isNull(source)) {
         pg.setValue(null);
       }
-      pg.setValue(objectMapper.writeValueAsString(source));
+      pg.setValue(this.objectMapper.writeValueAsString(source));
       return pg;
     } catch (SQLException | JsonProcessingException e) {
       log.error("convert error from:{} to:{} value:{}", sourceType, targetType, source, e);

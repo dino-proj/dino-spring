a84d3c621c40f3d7582b6740a25fa45e9f282d07
Cody Lu <tuuboo@gmail.com>
2024-01-17 20:57:12 +0800

    增加便利工具

--- dino-spring-auth/src/main/java/org/dinospring/auth/DinoAuth.java
+++ dino-spring-auth/src/main/java/org/dinospring/auth/DinoAuth.java
@@ -47,10 +47,134 @@ public class DinoAuth {
     RESOURCES.put(AUTH_SESSION_KEY, authSession);
   }
 
+  /**
+   * 是否登录
+   * @return
+   */
   public static boolean isLogin() {
     var authSession = getAuthSession();
     if (Objects.nonNull(authSession)) {
       return authSession.isLogin();
+    }
+    return false;
+  }
+
+  /**
+   * 当前登录是否是指定类型
+   * @return
+   */
+  public static boolean isLoginAs(String userType) {
+    var authSession = getAuthSession();
+    if (Objects.nonNull(authSession)) {
+      return authSession.isLoginAs(userType);
+    }
+    return false;
+  }
+
+  /**
+   * 当前登录用户是否拥有指定权限
+   * @return true:拥有指定权限，false:未登录或未拥有指定权限
+   */
+  public static boolean hasPermission(String permission) {
+    var authSession = getAuthSession();
+    if (Objects.nonNull(authSession)) {
+      return authSession.hasPermission(permission);
+    }
+    return false;
+  }
+
+  /**
+  * 当前登录用户是否拥有指定权限
+  * @return true:拥有指定权限，false:未登录或未拥有指定权限
+  */
+  public static boolean hasPermission(Permission permission) {
+    var authSession = getAuthSession();
+    if (Objects.nonNull(authSession)) {
+      return authSession.hasPermission(permission);
+    }
+    return false;
+  }
+
+  /**
+  * 当前登录用户是否拥有指定权限中的任意一个
+  * @return true:拥有指定权限，false:未登录或未拥有指定权限
+  */
+  public static boolean hasAnyPermission(String... permissions) {
+    var authSession = getAuthSession();
+    if (Objects.nonNull(authSession)) {
+      return authSession.hasAnyPermission(permissions);
+    }
+    return false;
+  }
+
+  /**
+  * 当前登录用户是否拥有指定权限中的任意一个
+  * @return true:拥有指定权限，false:未登录或未拥有指定权限
+  */
+  public static boolean hasAnyPermission(Permission... permissions) {
+    var authSession = getAuthSession();
+    if (Objects.nonNull(authSession)) {
+      return authSession.hasAnyPermission(permissions);
+    }
+    return false;
+  }
+
+  /**
+   * 当前登录用户是否拥有指定权限中的所有
+   * @return true:拥有指定权限，false:未登录或未拥有指定权限
+   */
+  public static boolean hasAllPermission(String... permissions) {
+    var authSession = getAuthSession();
+    if (Objects.nonNull(authSession)) {
+      return authSession.hasAllPermission(permissions);
+    }
+    return false;
+  }
+
+  /**
+  * 当前登录用户是否拥有指定权限中的所有
+  * @return true:拥有指定权限，false:未登录或未拥有指定权限
+  */
+  public static boolean hasAllPermission(Permission... permissions) {
+    var authSession = getAuthSession();
+    if (Objects.nonNull(authSession)) {
+      return authSession.hasAllPermission(permissions);
+    }
+    return false;
+  }
+
+  /**
+   * 当前登录用户是否拥有指定角色
+   * @return true:拥有指定角色，false:未登录或未拥有指定角色
+   */
+  public static boolean hasRole(String role) {
+    var authSession = getAuthSession();
+    if (Objects.nonNull(authSession)) {
+      return authSession.hasRole(role);
+    }
+    return false;
+  }
+
+  /**
+   * 当前登录用户是否拥有指定角色中的任意一个
+   * @return true:拥有指定角色，false:未登录或未拥有指定角色
+   */
+  public static boolean hasAnyRole(String... roles) {
+    var authSession = getAuthSession();
+    if (Objects.nonNull(authSession)) {
+      return authSession.hasAnyRole(roles);
+    }
+    return false;
+  }
+
+  /**
+   * 当前登录用户是否拥有指定角色中的所有
+   * @return true:拥有指定角色，false:未登录或未拥有指定角色
+   */
+  public static boolean hasAllRole(String... roles) {
+    var authSession = getAuthSession();
+    if (Objects.nonNull(authSession)) {
+      return authSession.hasAllRole(roles);
     }
     return false;
   }

51265ef672031335776a043e995f7991d712f80f
Cody Lu <tuuboo@gmail.com>
2024-11-05 18:55:50 +0800

    Refactor AliSmsService to update sendTemplateSms method signature and handle templateParams as Collection

--- dino-spring-core/src/main/java/org/dinospring/core/modules/sms/impl/AliSmsService.java
+++ dino-spring-core/src/main/java/org/dinospring/core/modules/sms/impl/AliSmsService.java
@@ -3,20 +3,21 @@
 
 package org.dinospring.core.modules.sms.impl;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
+import org.apache.commons.lang3.StringUtils;
 import org.dinospring.core.modules.sms.config.SmsModuleProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
-// import com.aliyun.auth.credentials.Credential;
-// import com.aliyun.auth.credentials.provider.StaticCredentialProvider;
-// import com.aliyun.sdk.service.dysmsapi20170525.AsyncClient;
-// import com.aliyun.sdk.service.dysmsapi20170525.models.SendSmsRequest;
+import com.aliyun.dysmsapi20170525.Client;
+import com.aliyun.dysmsapi20170525.models.SendSmsRequest;
+import com.aliyun.teaopenapi.models.Config;
 import com.google.gson.Gson;
 
-// import darabonba.core.client.ClientOverrideConfiguration;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -36,9 +37,9 @@ public class AliSmsService extends SmsServiceBase {
 
   @Override
   public Collection<String> sendTemplateSms(Collection<String> mobiles, String templateId,
-      Collection<String> templateParams, String signName) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'sendTemplateSms'");
+      Object templateParams, String signName) {
+    signName = StringUtils.defaultIfBlank(signName, this.smsModuleProperties.getAli().getSignName());
+    return this.doSendTemplateSms(mobiles, templateId, templateParams, signName);
   }
 
   @Override
@@ -46,58 +47,71 @@ public class AliSmsService extends SmsServiceBase {
     throw new UnsupportedOperationException("Unimplemented method 'sendSms'");
   }
 
-  // private Collection<String> doSendTemplateSms(Collection<String> mobiles, String templateId,
-  //     Collection<String> templateParams, String signName) {
+  private Collection<String> doSendTemplateSms(Collection<String> mobiles, String templateId,
+      Object templateParams, String signName) {
 
-  //   var failedMobiles = new ArrayList<String>();
-  //   var aliSmsProperties = this.smsModuleProperties.getAli();
+    var failedMobiles = new ArrayList<String>();
+    var aliSmsProperties = this.smsModuleProperties.getAli();
 
-  // 实例化一个认证对象，入参需要传入阿里云账户密钥对accessKeyId，accessKeySecret
-  // var cred = Credential.builder().accessKeyId(aliSmsProperties.getAccessKeyId())
-  //     .accessKeySecret(aliSmsProperties.getAccessKeySecret()).build();
+    // 实例化一个认证对象，入参需要传入阿里云账户密钥对accessKeyId，accessKeySecret
+    var config = new Config()
+        // 设置endpoint
+        .setEndpoint(StringUtils.defaultIfBlank(aliSmsProperties.getEndpoint(), "dysmsapi.aliyuncs.com"))
+        // 配置 AccessKey ID
+        .setAccessKeyId(aliSmsProperties.getAccessKeyId())
+        // 配置 AccessKey Secret，请确保代码运行环境设置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_SECRET。
+        .setAccessKeySecret(aliSmsProperties.getAccessKeySecret());
 
-  // var credentialProvider = StaticCredentialProvider.create(cred);
+    // 实例化一个客户端构造器
+    Client client;
+    try {
+      client = new Client(config);
 
-  // // 实例化一个客户端构造器
-  // var clientBuilder = AsyncClient.builder();
-  // clientBuilder.credentialsProvider(credentialProvider);
-  // clientBuilder.region(aliSmsProperties.getRegion());
-  // if (StringUtils.isNotBlank(aliSmsProperties.getEndpoint())) {
-  //   clientBuilder.overrideConfiguration(
-  //       ClientOverrideConfiguration.create().setEndpointOverride(aliSmsProperties.getEndpoint()));
-  // }
+      // 实例化一个请求Sms对象
+      var smsRequest = new SendSmsRequest();
+      smsRequest.setPhoneNumbers(StringUtils.join(mobiles, ","));
+      smsRequest.setSignName(signName);
+      smsRequest.setTemplateCode(templateId);
+      smsRequest.setTemplateParam(paramToJsonStr(templateParams));
 
-  // // 实例化一个请求Sms对象
-  // var smsRequestBuilder = SendSmsRequest.builder();
-  // smsRequestBuilder.s.setSmsSdkAppId(aliSmsProperties.getAppId());
+      // 设置extendCode
+      if (StringUtils.isNotBlank(aliSmsProperties.getExtendCode())) {
+        smsRequest.setSmsUpExtendCode(aliSmsProperties.getExtendCode());
+      }
 
-  // smsRequestBuilder.phoneNumbers(StringUtils.join(mobiles, ","));
-  // smsRequestBuilder.templateCode(templateId);
-  // smsRequestBuilder.templateParam(templateParams.toArray(new String[templateParams.size()]));
-  // smsRequestBuilder.signName(signName);
+      // 获取响应对象
+      var sendSmsResponse = client.sendSms(smsRequest);
 
-  // // 设置extendCode
-  // if (StringUtils.isNotBlank(aliSmsProperties.getExtendCode())) {
-  //   smsRequestBuilder.smsUpExtendCode(aliSmsProperties.getExtendCode());
-  // }
+      if (sendSmsResponse.getStatusCode() != 0) {
+        log.error("短信发送失败: {}", sendSmsResponse);
+        failedMobiles.addAll(mobiles);
+      }
+    } catch (Exception e) {
+      log.error("短信发送失败", e);
+      failedMobiles.addAll(mobiles);
+    }
 
-  // try (AsyncClient client = clientBuilder.build()) {
+    return failedMobiles;
+  }
 
-  // }
+  private String paramToJsonStr(Object params) {
+    // 如果是Collection或array, 则转为Index-Value的Map
 
-  // }
+    if (params instanceof Collection) {
 
-  // private String paramToIndexedJsonStr(String[] params) {
-  //   if (params == null || params.length == 0) {
-  //     return "";
-  //   }
-  //   // var paramsMap = Arrays.stream(params).collect(Collectors.toMap(String::valueOf, String::valueOf));
-  //   // gson.toJson(params)
-  //   // StringBuilder jsonStringBuilder = new StringBuilder();
-  //   // jsonStringBuilder.append('{');
-  //   // for(int idx = 0; idx < params.length; idx++) {
-  //   //   jsonStringBuilder.append("\"").append(idx).append("\":\"").append(StringTokenizer. params[idx]).append("\",");
-  //   // }
-  // }
-
+      var paramsMap = new HashMap<String, Object>();
+      for (var i = 0; i < ((Collection<?>) params).size(); i++) {
+        paramsMap.put(String.valueOf(i + 1), ((Collection<?>) params).toArray()[i]);
+      }
+      return gson.toJson(paramsMap);
+    } else if (params.getClass().isArray()) {
+      var paramsMap = new HashMap<String, Object>();
+      for (var i = 0; i < ((Object[]) params).length; i++) {
+        paramsMap.put(String.valueOf(i + 1), ((Object[]) params)[i]);
+      }
+      return gson.toJson(paramsMap);
+    } else {
+      return gson.toJson(params);
+    }
+  }
 }

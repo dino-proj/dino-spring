0a7f004bc00160c495f91204a8a386ed0b3d64fc
Cody Lu <tuuboo@gmail.com>
2024-11-19 15:28:40 +0800

    修改异常处理逻辑，并添加注释文档

--- dino-spring-core/src/main/java/cn/dinodev/spring/core/modules/importandexport/dataimport/DataImport.java
+++ dino-spring-core/src/main/java/cn/dinodev/spring/core/modules/importandexport/dataimport/DataImport.java
@@ -5,7 +5,6 @@ package cn.dinodev.spring.core.modules.importandexport.dataimport;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -20,7 +19,6 @@ import com.alibaba.excel.read.listener.ReadListener;
 import com.alibaba.excel.read.metadata.ReadSheet;
 
 import cn.dinodev.spring.commons.utils.LambdaUtils;
-
 import lombok.SneakyThrows;
 
 /**
@@ -29,6 +27,21 @@ import lombok.SneakyThrows;
  */
 public class DataImport {
 
+  /**
+   * 读取Excel文件并处理数据
+   *
+   * @param <T> 数据类型
+   * @param in 输入流
+   * @param cls 数据类型的Class对象
+   * @param cb 处理数据的回调函数
+   * @throws IOException 如果读取文件时发生错��
+   *
+   * 示例：
+   * InputStream in = new FileInputStream("example.xlsx");
+   * DataImport.doRead(in, ExampleData.class, dataList -> {
+   *     // 处理数据
+   * });
+   */
   public static <T> void doRead(InputStream in, Class<T> cls, Consumer<List<T>> cb) throws IOException {
     EasyExcel.read(in, cls, new ReadListener<T>() {
       private List<T> list = new LinkedList<>();
@@ -42,75 +55,115 @@ public class DataImport {
       @Override
       public void doAfterAllAnalysed(AnalysisContext context) {
         cb.accept(list);
-        in.close();
       }
     }).sheet().doRead();
   }
 
+  /**
+   * 读取Excel文件并处理数据
+   *
+   * @param <T> 数据类型
+   * @param pathName 文件路径
+   * @param cls 数据类型的Class对象
+   * @param cb 处理数据的回调函数
+   * @throws IOException 如果读取文件时发生错误
+   *
+   * 示例：
+   * DataImport.doRead("example.xlsx", ExampleData.class, dataList -> {
+   *     // 处理数据
+   * });
+   */
   public static <T> void doRead(String pathName, Class<T> cls, Consumer<List<T>> cb) throws IOException {
-    InputStream in = null;
-    try {
-      in = new FileInputStream(new File(pathName));
+
+    try (InputStream in = new FileInputStream(new File(pathName))) {
       doRead(in, cls, cb);
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
     }
+
   }
 
+  /**
+   * 读取多个Sheet并处理数据
+   *
+   * @param <T> 数据类型
+   * @param in 输入流
+   * @param list 数据类型的Class对象列表
+   * @param cb 处理数据的回调函数
+   * @throws IOException 如果读取文件时发生错误
+   *
+   * 示例：
+   * InputStream in = new FileInputStream("example.xlsx");
+   * List<Class<ExampleData>> list = Arrays.asList(ExampleData.class);
+   * DataImport.repeatedRead(in, list, dataList -> {
+   *     // 处理数据
+   * });
+   */
   public static <T> void repeatedRead(InputStream in, List<Class<T>> list, Consumer<List<T>> cb)
       throws IOException {
-    try {
-      ExcelReader excelReader = EasyExcel.read(in).build();
-      analysis(excelReader, list, cb);
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      in.close();
-    }
+
+    ExcelReader excelReader = EasyExcel.read(in).build();
+    analysis(excelReader, list, cb);
 
   }
 
+  /**
+   * 读取多个Sheet并处理数据
+   *
+   * @param <T> 数据类型
+   * @param pathName 文件路径
+   * @param list 数据类型的Class对象列表
+   * @param cb 处理数据的回调函数
+   * @throws IOException 如果读取文件时发生错误
+   *
+   * 示例：
+   * List<Class<ExampleData>> list = Arrays.asList(ExampleData.class);
+   * DataImport.repeatedRead("example.xlsx", list, dataList -> {
+   *     // 处理数据
+   * });
+   */
   public static <T> void repeatedRead(String pathName, List<Class<T>> list, Consumer<List<T>> cb) throws IOException {
-    InputStream in = null;
-    try {
-      in = new FileInputStream(new File(pathName));
+
+    try (InputStream in = new FileInputStream(new File(pathName))) {
       repeatedRead(in, list, cb);
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } finally {
-      in.close();
     }
+
   }
 
+  /**
+   * 分析Excel文件并处理数据
+   *
+   * @param <T> 数据类型
+   * @param excelReader Excel读取器
+   * @param list 数据类型的Class对象列表
+   * @param cb 处理数据的回调函数
+   *
+   * 示例：
+   * ExcelReader excelReader = EasyExcel.read(new FileInputStream("example.xlsx")).build();
+   * List<Class<ExampleData>> list = Arrays.asList(ExampleData.class);
+   * DataImport.analysis(excelReader, list, dataList -> {
+   *     // 处理数据
+   * });
+   */
   private static <T> void analysis(ExcelReader excelReader, List<Class<T>> list, Consumer<List<T>> cb) {
-    try {
-      List<ReadSheet> readSheets = new ArrayList<>();
-      LambdaUtils.forEach(list, (index, item) -> {
-        ReadSheet readSheet = EasyExcel.readSheet(index).head(item).registerReadListener(new ReadListener<T>() {
-          List<T> list = new LinkedList<>();
 
-          @Override
-          public void invoke(T data, AnalysisContext context) {
-            list.add(data);
-          }
+    List<ReadSheet> readSheets = new ArrayList<>();
+    LambdaUtils.forEach(list, (index, item) -> {
+      ReadSheet readSheet = EasyExcel.readSheet(index).head(item).registerReadListener(new ReadListener<T>() {
+        List<T> list = new LinkedList<>();
 
-          @SneakyThrows
-          @Override
-          public void doAfterAllAnalysed(AnalysisContext context) {
-            cb.accept(list);
-          }
-        }).build();
-        readSheets.add(readSheet);
-      });
-      excelReader.read(readSheets);
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      if (excelReader != null) {
-        // 这里千万别忘记关闭，读的时候会创建临时文件，到时磁盘会崩的
-        excelReader.finish();
-      }
-    }
+        @Override
+        public void invoke(T data, AnalysisContext context) {
+          list.add(data);
+        }
+
+        @SneakyThrows
+        @Override
+        public void doAfterAllAnalysed(AnalysisContext context) {
+          cb.accept(list);
+        }
+      }).build();
+      readSheets.add(readSheet);
+    });
+    excelReader.read(readSheets);
 
   }
 

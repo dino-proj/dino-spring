c2ee693bb5b14a85340df11dce70ed33f031b096
Cody Lu <tuuboo@gmail.com>
2024-11-19 18:48:18 +0800

    为 SchemaManager 类中的 jdbcOperations 字段添加 @SuppressWarnings 注解，避免未使用警告

--- dino-spring-data/src/main/java/cn/dinodev/spring/data/schema/SchemaManager.java
+++ dino-spring-data/src/main/java/cn/dinodev/spring/data/schema/SchemaManager.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class SchemaManager {
 
+  @SuppressWarnings("unused")
   private JdbcOperations jdbcOperations;
   private List<TableDef> tables = new ArrayList<>();
 

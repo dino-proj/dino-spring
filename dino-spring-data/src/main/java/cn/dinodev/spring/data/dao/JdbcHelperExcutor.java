a4e48cde24573a9557bb8f030a8e369178a030a2
Cody Lu <tuuboo@gmail.com>
2024-11-21 16:24:43 +0800

    为 JdbcHelperExcutor 接口添加多个更新方法，支持更新指定列及版本控制

--- dino-spring-data/src/main/java/cn/dinodev/spring/data/dao/JdbcHelperExcutor.java
+++ dino-spring-data/src/main/java/cn/dinodev/spring/data/dao/JdbcHelperExcutor.java
@@ -5,9 +5,13 @@ package cn.dinodev.spring.data.dao;
 
 import java.util.Map;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.transaction.annotation.Transactional;
+
 import cn.dinodev.spring.data.sql.builder.DeleteSqlBuilder;
 import cn.dinodev.spring.data.sql.builder.UpdateSqlBuilder;
-import org.springframework.data.jdbc.repository.query.Modifying;
+import jakarta.annotation.Nullable;
 
 /**
  *
@@ -57,10 +61,96 @@ public interface JdbcHelperExcutor<T, K> {
    * @param value
    * @return
    */
+  @Transactional(rollbackFor = Exception.class)
   @Modifying
   default boolean updateById(K id, String column, Object value) {
     return updateById(id, Map.of(column, value));
   }
+
+  /**
+   * 更新指定列
+   * @param id
+   * @param column1
+   * @param value1
+   * @param column2
+   * @param value2
+   * @return
+   */
+  @Transactional(rollbackFor = Exception.class)
+  @Modifying
+  default boolean updateById(K id, String column1, Object value1, String column2, Object value2) {
+    return updateById(id, Map.of(column1, value1, column2, value2));
+  }
+
+  /**
+   * 更新指定列
+   * @param id
+   * @param column1
+   * @param value1
+   * @param column2
+   * @param value2
+   * @param column3
+   * @param value3
+   * @return
+   */
+  @Transactional(rollbackFor = Exception.class)
+  @Modifying
+  default boolean updateById(K id, String column1, Object value1, String column2, Object value2, String column3,
+      Object value3) {
+    return updateById(id, Map.of(column1, value1, column2, value2, column3, value3));
+  }
+
+  /**
+   * 更新指定列
+   * @param id
+   * @param column1
+   * @param value1
+   * @param column2
+   * @param value2
+   * @param column3
+   * @param value3
+   * @param column4
+   * @param value4
+   * @return
+   */
+  @Transactional(rollbackFor = Exception.class)
+  @Modifying
+  default boolean updateById(K id, String column1, Object value1, String column2, Object value2, String column3,
+      Object value3, String column4, Object value4) {
+    return updateById(id, Map.of(column1, value1, column2, value2, column3, value3, column4, value4));
+  }
+
+  /**
+   * 更新指定列
+   * @param id
+   * @param column1
+   * @param value1
+   * @param column2
+   * @param value2
+   * @param column3
+   * @param value3
+   * @param column4
+   * @param value4
+   * @param column5
+   * @param value5
+   * @return
+   */
+  @Transactional(rollbackFor = Exception.class)
+  @Modifying
+  default boolean updateById(K id, String column1, Object value1, String column2, Object value2, String column3,
+      Object value3, String column4, Object value4, String column5, Object value5) {
+    return updateById(id, Map.of(column1, value1, column2, value2, column3, value3, column4, value4, column5, value5));
+  }
+
+  /**
+   * 更新指定列
+   * @param id
+   * @param columnValue
+   * @return
+   */
+  @Transactional(rollbackFor = Exception.class)
+  @Modifying
+  boolean updateById(K id, Map<String, Object> columnValue);
 
   /**
    * 更新指定列，并判断版本，如果版本不对，则不更新，如果版本正确，则版本自动+1
@@ -70,23 +160,10 @@ public interface JdbcHelperExcutor<T, K> {
    * @param version
    * @return
    */
+  @Transactional(rollbackFor = Exception.class)
   @Modifying
   default boolean updateByIdWithVersion(K id, String column, Object value, Number version) {
     return updateByIdWithVersion(id, Map.of(column, value), version);
-  }
-
-  /**
-   * 更新指定列
-   * @param id
-   * @param column1
-   * @param value1
-   * @param column2
-   * @param value2
-   * @return
-   */
-  @Modifying
-  default boolean updateById(K id, String column1, Object value1, String column2, Object value2) {
-    return updateById(id, Map.of(column1, value1, column2, value2));
   }
 
   /**
@@ -99,6 +176,7 @@ public interface JdbcHelperExcutor<T, K> {
    * @param version
    * @return
    */
+  @Transactional(rollbackFor = Exception.class)
   @Modifying
   default boolean updateByIdWithVersion(K id, String column1, Object value1, String column2, Object value2,
       Number version) {
@@ -106,13 +184,69 @@ public interface JdbcHelperExcutor<T, K> {
   }
 
   /**
-   * 更新指定列
+   * 更新指定列，并判断版本，如果版本不对，则不更新，如果版本正确，则版本自动+1
    * @param id
-   * @param columnValue
+   * @param column1
+   * @param value1
+   * @param column2
+   * @param value2
+   * @param column3
+   * @param value3
+   * @param version
    * @return
    */
+  @Transactional(rollbackFor = Exception.class)
   @Modifying
-  boolean updateById(K id, Map<String, Object> columnValue);
+  default boolean updateByIdWithVersion(K id, String column1, Object value1, String column2, Object value2,
+      String column3, Object value3, Number version) {
+    return updateByIdWithVersion(id, Map.of(column1, value1, column2, value2, column3, value3), version);
+  }
+
+  /**
+   * 更新指定列，并判断版本，如果版本不对，则不更新，如果版本正确，则版本自动+1
+   * @param id
+   * @param column1
+   * @param value1
+   * @param column2
+   * @param value2
+   * @param column3
+   * @param value3
+   * @param column4
+   * @param value4
+   * @param version
+   * @return
+   */
+  @Transactional(rollbackFor = Exception.class)
+  @Modifying
+  default boolean updateByIdWithVersion(K id, String column1, Object value1, String column2, Object value2,
+      String column3, Object value3, String column4, Object value4, Number version) {
+    return updateByIdWithVersion(id, Map.of(column1, value1, column2, value2, column3, value3, column4, value4),
+        version);
+  }
+
+  /**
+   * 更新指定列，并判断版本，如果版本不对，则不更新，如果版本正确，则版本自动+1
+   * @param id
+   * @param column1
+   * @param value1
+   * @param column2
+   * @param value2
+   * @param column3
+   * @param value3
+   * @param column4
+   * @param value4
+   * @param column5
+   * @param value5
+   * @param version
+   * @return
+   */
+  @Transactional(rollbackFor = Exception.class)
+  @Modifying
+  default boolean updateByIdWithVersion(K id, String column1, Object value1, String column2, Object value2,
+      String column3, Object value3, String column4, Object value4, String column5, Object value5, Number version) {
+    return updateByIdWithVersion(id, Map.of(column1, value1, column2, value2, column3, value3, column4, value4, column5,
+        value5), version);
+  }
 
   /**
    * 更新指定列，并判断版本，如果版本不对，则不更新，如果版本正确，则版本自动+1
@@ -121,22 +255,40 @@ public interface JdbcHelperExcutor<T, K> {
    * @param version
    * @return
    */
+  @Transactional(rollbackFor = Exception.class)
   @Modifying
   boolean updateByIdWithVersion(K id, Map<String, Object> columnValue, Number version);
 
   /**
-   * 修改
+   * 执行更新sql
    * @param updateSqlBuilder
-   * @return
+   * @return 影响行数
    */
+  @Transactional(rollbackFor = Exception.class)
   @Modifying
-  long update(UpdateSqlBuilder updateSqlBuilder);
+  default int update(UpdateSqlBuilder updateSqlBuilder) {
+    return update(updateSqlBuilder.getSql(), updateSqlBuilder.getParams());
+  }
 
   /**
-   * 删除
+   * 执行更新sql，返回影响行数
+   * @param sql sql语句
+   * @param args sql参数
+   * @return 影响行数
+   * @throws DataAccessException
+   */
+  @Transactional(rollbackFor = Exception.class)
+  @Modifying
+  int update(String sql, @Nullable Object... args) throws DataAccessException;
+
+  /**
+   * 执行删除sql
    * @param deleteSqlBuilder
    * @return
    */
+  @Transactional(rollbackFor = Exception.class)
   @Modifying
-  long delete(DeleteSqlBuilder deleteSqlBuilder);
+  default int delete(DeleteSqlBuilder deleteSqlBuilder) {
+    return update(deleteSqlBuilder.getSql(), deleteSqlBuilder.getParams());
+  }
 }

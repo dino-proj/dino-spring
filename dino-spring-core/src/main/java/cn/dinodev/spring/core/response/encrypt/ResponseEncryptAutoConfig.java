09c1492722b1c679a6b2601a533aef8c18d9eb4b
Cody Lu <tuuboo@gmail.com>
2025-09-18 00:25:28 +0800

    新增日志记录功能以便调试响应数据加密配置

--- dino-spring-core/src/main/java/cn/dinodev/spring/core/response/encrypt/ResponseEncryptAutoConfig.java
+++ dino-spring-core/src/main/java/cn/dinodev/spring/core/response/encrypt/ResponseEncryptAutoConfig.java
@@ -8,6 +8,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 响应数据加密自动配置
  * @author Cody Lu
@@ -16,11 +18,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(ResponseEncryptProperties.class)
+@Slf4j
 public class ResponseEncryptAutoConfig {
 
   @Bean
   @ConditionalOnProperty(prefix = ResponseEncryptProperties.PREFIX, name = "enabled", havingValue = "true", matchIfMissing = true)
   public ResponseDataEncryptor responseDataEncryptor(ResponseEncryptProperties props) {
+    // 打印日志，方便调试
+    log.debug("---->> response-encrypt: enabled， aesKey length: {}",
+        props.getAesKey() == null ? 0 : props.getAesKey().length());
+
     // 初始化默认的加密器, 这里可以根据配置选择不同的加密实现
     return new ResponseDataAesEncryptor(props.getAesKey());
   }
